fbfef29fdcf7361b3a74df808388ab6a92ffeb57
RoxanaCanciani <roxicanciani@gmail.com>
2022-12-28 23:33:27 -0300

    clase 04 completada

--- components/checkComplete.js
+++ components/checkComplete.js
@@ -1,13 +1,13 @@
 const checkComplete = () => {
   const i = document.createElement('i');
-  i.classList.add('far', 'fa-check-square', 'icon');
+  i.classList.add('far', 'fa-check-square', 'icon');//add: para agregar una clase, remove: para quitar
   i.addEventListener('click', completeTask);
   return i;
 };
 // Immediately invoked function expression IIFE
 const completeTask = (event) => {
   const element = event.target;
-  element.classList.toggle('fas');
+  element.classList.toggle('fas');// toggle: si existe la clase la quito, si no existe la agrego
   element.classList.toggle('completeIcon');
   element.classList.toggle('far');
 };
