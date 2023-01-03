c008456fd5742bdf01b90af05e72932e57720565
RoxanaCanciani <roxicanciani@gmail.com>
2023-01-03 15:47:43 -0300

    clase 05 completada

---
+++ services/date.js
@@ -0,0 +1,19 @@
+export const uniqueDate = (tasks)=>{
+   
+ const unique=[];
+
+ tasks.forEach(task=>{
+    if(!unique.includes(task.dateFormat)){
+        unique.push(task.dateFormat)
+    }
+});
+return unique;
+}
+
+export const orderDates = (dates)=>{
+return dates.sort((a,b) =>{
+    const firstDate = moment(a, 'DD/MM/YYYY');
+    const lastDate = moment(b, 'DD/MM/YYYY');
+    return firstDate - lastDate;
+})
+}
